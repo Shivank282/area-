cf99a59ca1cb52578df74b7e3ebe5d82992316e5
Shivank282 <shivankbhargav98@gmail.com>
2024-10-01 11:51:35 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float r,pie=3.14,cir;
+
+    printf("enter the value of radius");
+    scanf("%f",&r);
+cir=pie*r*r;
+printf("%f",cir);
+
+return 0;
+
+
+}
